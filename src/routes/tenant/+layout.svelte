def1497d703a744c2b4e183906e559d0880dde72
phufaphu <pphufaphu@gmail.com>
2025-03-03 01:15:49 +0700

    fix: layout tenant

--- src/routes/tenant/+layout.svelte
+++ src/routes/tenant/+layout.svelte
@@ -3,9 +3,9 @@
   let { children } = $props();
 </script>
 
-<div class="flex min-h-screen absolute">
+<div class="flex min-h-screen">
   <div
-    class="sidebar bg-[#404040] text-white py-8 m-5 flex flex-col rounded-2xl items-center justify-center w-1/12 "
+    class="sidebar bg-[#404040] text-white py-8 my-5 mx-4 flex flex-col rounded-2xl items-center justify-center w-[12%] "
   >
     <div>
       <div class="flex flex-col items-center mb-4">
@@ -37,7 +37,7 @@
 
   <div
     id="rest"
-    class="flex-1 bg-[#F2F2F2] bg-opacity-[0.85] m-5 p-4 rounded-2xl pl-[200px]"
+    class="flex-1 my-5"
   >
     {@render children()}
   </div>
--- src/routes/tenant/waiting/+page.svelte
+++ src/routes/tenant/waiting/+page.svelte
@@ -2,12 +2,11 @@
     /** @type {{ data: import('./$types').PageData }} */
     let { data } = $props();
 </script>
-
-<div class="flex flex-col items-end justify-center h-screen mr-5">
     <!-- ไทย -->
-    <div class="bg-[rgba(242,242,242,0.8)] rounded-2xl w-[90%] h-[95%] shadow-lg flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold">รอการอนุมัติ</h1>
-        <p class="text-lg">บัญชีของคุณกำลังรอการอนุมัติจากเจ้าของที่พัก</p>
-        <p class="text-lg">ขอบคุณสำหรับความอดทนของคุณ</p>
+    <div class="flex justify-center items-center flex-col h-full">
+        <div class="p-8 flex flex-col items-center">
+            <h1 class="text-4xl font-bold">รอการอนุมัติ</h1>
+            <p class="text-lg">บัญชีของคุณกำลังรอการอนุมัติจากเจ้าของที่พัก</p>
+            <p class="text-lg">ขอบคุณสำหรับความอดทนของคุณ</p>
+        </div>
     </div>
-</div>
