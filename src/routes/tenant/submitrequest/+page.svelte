c9d45cc36b4f54aa89cef3dc6ae7831942905dfe
AromAromE <138848620+AromAromE@users.noreply.github.com>
2025-03-03 01:20:34 +0700

    userprofile and notification + submitrequest

---
+++ src/routes/tenant/submitrequest/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    /** @type {{ data: import('./$types').PageData }} */
+    let { data } = $props();
+</script>
+
+<div class="w-full h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center p-6" style="background-image: url('/images/bg.png');">
+    <div class="w-[90%] h-[90%] p-10 flex flex-col">
+        <div class="w-full text-left mb-6">
+            <p class="text-[#F2F2F2] text-2xl font-semibold">
+                <span class="text-[#F2F2F2] mr-2">|</span>ส่งคำร้องซ่อมบำรุง
+            </p>
+        </div>
+
+        <div class="flex-grow flex flex-col items-center"> 
+            <div class="mb-6 w-[60%]"> 
+                <p class="text-[#F2F2F2] text-xl font-semibold mb-2">เลือกประเภท</p>
+                <div class="flex justify-between gap-4">
+                    <button class="w-[23%] border border-[#404040] rounded-lg px-6 py-3 text-[#404040] bg-[#D9D9D9] font-medium hover:bg-[#404040] hover:text-[#F2F2F2] shadow-md">ไฟฟ้า</button>
+                    <button class="w-[23%] border border-[#404040] rounded-lg px-6 py-3 text-[#404040] bg-[#D9D9D9] font-medium hover:bg-[#404040] hover:text-[#F2F2F2] shadow-md">น้ำปะปา</button>
+                    <button class="w-[23%] border border-[#404040] rounded-lg px-6 py-3 text-[#404040] bg-[#D9D9D9] font-medium hover:bg-[#404040] hover:text-[#F2F2F2] shadow-md">อุปกรณ์</button>
+                    <button class="w-[23%] border border-[#404040] rounded-lg px-6 py-3 text-[#404040] bg-[#D9D9D9] font-medium hover:bg-[#404040] hover:text-[#F2F2F2] shadow-md">อื่นๆ</button>
+                </div>
+            </div>
+
+            <div class="mb-6 w-[60%] flex-grow flex flex-col"> 
+                <p class="text-[#F2F2F2] text-xl font-semibold mb-2">รายละเอียด</p>
+                <textarea class="w-full h-full border bg-[#D9D9D9] border-[#404040] rounded-lg p-3 text-[#404040] focus:outline-none focus:ring focus:border-[#000000] placeholder:text-[#A4A5A6]" placeholder="แจ้งรายละเอียด..."></textarea>
+            </div>
+
+            <div class="w-full flex justify-center"> 
+                <button class="w-[10%] py-3 mt-9 rounded-[25px] text-xl cursor-pointer bg-[#404040] border-none text-[#D9D9D9] shadow-md">ส่งคำร้อง</button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/routes/tenant/userprofile/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    let name = "ปทิตตา ดวงแก้ว"; 
+    let email = "patittadua@gmail.com";
+    let password = "Pramandanijjanukor2546";
+    let history = [
+        { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+        { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+        { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+        { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+        { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+        { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+
+    ];
+</script>
+
+<div class="w-full min-h-screen bg-cover bg-center bg-no-repeat" style="background-image: url('/images/bg.png');">
+    <div class="w-full h-screen flex flex-col items-center justify-center">
+        <div class="w-[85%] h-[90%] pt-16 flex flex-col items-center px-10">
+            <img src="/images/Icon.svg" alt="icon" class="mb-6 w-24 h-24 fill-[#F2F2F2]" />
+            <p class="text-[#F2F2F2] font-normal font-jeju text-4xl">{name}</p>
+            <p class="text-[#F2F2F2] font-normal font-jeju text-2xl mb-10">{email}</p>
+
+            <div class="w-full max-w-2xl text-left mb-10">
+                <p class="text-[#F2F2F2] text-2xl font-normal leading-none">รหัสผ่าน</p>
+                <p class="text-[#F2F2F2] text-xl mt-2">{password}</p>
+            </div>
+
+            <div class="w-full max-w-2xl border-t border-gray-400 mb-8"></div>
+
+            <div class="w-full max-w-3xl">
+                <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-10">ประวัติการเช่า</p>
+                <div class="grid grid-cols-2 gap-6">
+                    {#each history as item}
+                    <div class="bg-gray-200 border-[#404040] shadow-md rounded-lg p-4 border-l-4 pl-4">
+                        <p class="text-black text-xl font-medium">{item.month} ยอดรวม {item.amount}</p>
+                        <p class="text-gray-600 text-lg">สถานะ: {item.status}</p>
+                    </div>
+                    {/each}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
