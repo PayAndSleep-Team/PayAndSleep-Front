3a7829bd89f63b49c2fa137ad40251ba8aa3ac21
phufaphu <pphufaphu@gmail.com>
2025-03-03 06:20:53 +0700

    refactor: enhance layout and functionality for notifications, submit request, and user profile pages

--- src/routes/tenant/notifications/+page.svelte
+++ src/routes/tenant/notifications/+page.svelte
@@ -11,13 +11,21 @@
     ];
 </script>
 
-<div class="w-full h-full flex justify-center p-6">
-    <div class="w-[90%] h-[90%] p-10 flex flex-col">
-        <div class="w-full text-left mb-6">
-            <p class="ml-16 mb-8 text-[#F2F2F2] text-2xl font-semibold">
-                <span class="text-[#F2F2F2] mr-2">|</span> การแจ้งเตือน
-            </p>
-        </div>
+<div class="">
+    <div class="flex items-center" style="margin: 2rem;">
+        <img src="/images/whiteRect.svg" alt="whiteRect">
+        <p class="text-2xl ml-2 text-[#F2F2F2]">การแจ้งเตือน</p>
+    </div>
+    <div class="flex flex-col gap-6 mx-20">
+        {#each notifications as notification}
+            <div class="flex items-center bg-[#D9D9D9] rounded-lg shadow-md p-4">
+                <img src="/images/bell.svg" alt="bell" class="w-8 h-8 mr-4" />
+                <p class="text-lg text-[#404040]">{notification}</p>
+            </div>
+        {/each}
+    </div>
+</div>
+<!-- <div class="w-full h-full flex justify-center p-6">
         <div class="flex-grow overflow-y-auto space-y-6">
             {#each notifications as notification}
                 <div class="ml-32 mr-32 flex h-[10%] items-center bg-[#D9D9D9] rounded-lg shadow-md p-4" style="background-color: #D9D9D9; margin-bottom: 43px;">
@@ -27,4 +35,4 @@
             {/each}
         </div>
     </div>
-</div>
+</div> -->
