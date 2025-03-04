d9c1378b0ab5c7d43f67e113809d93080d01609a
phufaphu <pphufaphu@gmail.com>
2025-03-05 04:51:17 +0700

    feat: update user profile page with improved input handling and layout adjustments

--- src/routes/tenant/userprofile/+page.svelte
+++ src/routes/tenant/userprofile/+page.svelte
@@ -14,9 +14,9 @@
     ];
   
     let isEditing = $state(false);
-    let tempName = $state("temp");
-    let tempEmail = $state("temp");
-    let tempPassword = $state("temp");
+    let tempName = $state("ปทิตตา ดวงแก้ว");
+    let tempEmail = $state("patittadua@gmail.com");
+    let tempPassword = $state("Pramandanijjanukor2546");
   
     onMount(async () => {
       try {
@@ -26,9 +26,9 @@
         });
         user = await res.json();
         profile = user.profile;
-        tempName = profile?.name || "temp";
-        tempEmail = user?.email || "temp";
-        tempPassword = user?.password || "temp";
+        tempName = profile?.name;
+        tempEmail = user?.email || "Enter Email";
+        tempPassword = user?.password || "Enter Password";
       } catch (error) {
         console.error("Error fetching message:", error);
       }
@@ -36,9 +36,9 @@
   
     function toggleEdit() {
       if (!isEditing) {
-        tempName = profile?.name || "temp";
-        tempEmail = user?.email || "temp";
-        tempPassword = user?.password || "temp";
+        tempName = profile?.name;
+        tempEmail = user?.email;
+        tempPassword = user?.password;
       } else {
         if (profile) {
           profile.name = tempName;
@@ -54,55 +54,58 @@
   
   <div class="w-full min-h-full relative">
     <div class="w-full h-full flex flex-col items-center justify-center">
-      <div class="w-[85%] h-[90%] pt-16 flex flex-col items-center px-10">
+      <div class="w-full pt-16 flex flex-col items-center px-10">
         <img
           src="/images/Icon.svg"
           alt="icon"
           class="mb-6 w-24 h-24 fill-[#F2F2F2]"
         />
         {#if isEditing}
-          <input class="mb-4 w-[10%] py-2.5 px-6 rounded-[25px] border-2 border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempName} />
+          <input class="mb-4 py-1 px-6 rounded-xl border-2  border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempName} placeholder="Enter Name"/>
         {:else}
           <p class="text-[#F2F2F2] font-normal font-jeju text-4xl">{tempName}</p>
         {/if}
   
         {#if isEditing}
-          <input class="w-[10%] py-2.5 px-6 rounded-[25px] border-2 border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempEmail} />
+          <input class="py-1 px-6 rounded-xl border-2 border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempEmail} placeholder="Enter Email"/>
         {:else}
           <p class="text-[#F2F2F2] font-normal font-jeju text-2xl mb-10">
             {tempEmail}
           </p>
         {/if}
   
-        <div class="w-full max-w-2xl text-left mb-10 relative">
+        <div class="w-full max-w-2xl text-left mb-10 relative mr-24">
           <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-4">
             รหัสผ่าน
           </p>
           {#if isEditing}
-            <input class="mb-4 w-full py-2.5 px-6 rounded-[25px] border-2 border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempPassword} />
+            <input class="mb-4 w-full py-1 px-6 rounded-xl border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempPassword} placeholder="Enter Password"/>
           {:else}
             <p class="text-[#F2F2F2] text-xl mb-4 pl-10">{tempPassword}</p>
           {/if}
           <div
-            class="absolute bottom-0 left-0 w-full border-t border-gray-400"
+            class="absolute bottom-0 left-0 w-full border-t-2"
           ></div>
         </div>
         <div class="w-full max-w-3xl">
-          <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-10">
+          <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-6">
             ประวัติการเช่า
           </p>
           <div class="grid grid-cols-2 gap-6">
             {#each history as item}
-              <div
-                class="bg-gray-200 border-[#404040] shadow-md rounded-lg p-4 border-l-4 pl-4"
-              >
-                <p class="text-black text-xl font-medium">
-                  {item.month} ยอดรวม {item.amount}
-                </p>
-                <p class="text-gray-600 text-lg">
-                  สถานะ: {item.status}
-                </p>
+            <div class="bg-white rounded-xl shadow overflow-hidden p-2">
+              <div class="flex">
+                <div><img src="/images/grayRect.svg" alt="" class="h-20"/></div>
+                <div class="flex flex-col justify-center">
+                  <div class="px-3 text-xl text-[#404040]">{item.month} ยอดรวม {item.amount}</div>
+                  <div
+                    class="px-3 text-xl text-[#404040]"
+                  >
+                    สภานะ: {item.status}
+                  </div>
+                </div>
               </div>
+            </div>
             {/each}
           </div>
         </div>
@@ -110,7 +113,7 @@
     </div>
       <button
         onclick={toggleEdit}
-        class="absolute bottom-4 right-4 bg-transparent border-none p-0"
+        class="absolute bottom-12 right-12 bg-transparent border-none p-0"
       >
           {#if isEditing}
               <img src="/images/confirm.svg" alt="Confirm" class="w-8 h-8" />
