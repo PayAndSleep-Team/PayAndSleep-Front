441f8b1062cdb690392a97c07ce319fd68742449
Bss-bass <sivapat13@gmail.com>
2025-03-05 23:17:52 +0700

    Add Back: in host/noti|room, tenant/noti|userprofile

--- src/routes/tenant/notifications/+page.svelte
+++ src/routes/tenant/notifications/+page.svelte
@@ -6,9 +6,27 @@
 
     let notifications = $state([]);
 
-    const markAsRead = (item) => {
-        item.isRead = true;
-    };
+    async function markAsRead(id) {
+        try {
+            const res = await fetch(`http://localhost:3000/api/read/notification`, {
+                method: "PUT",
+                credentials: "include",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id }),
+            });
+            const data = await res.json();
+            if (data.message === "อัพเดทสถานะการแจ้งเตือนสำเร็จ") {
+                notifications = notifications.map((notification) => {
+                    if (notification.id === id) {
+                        return { ...notification, status: "read" };
+                    }
+                    return notification;
+                });
+            }
+        } catch (error) {
+            console.error("Error fetching message:", error);
+        }
+    }
 
     onMount(async () => {
         try {
@@ -31,17 +49,17 @@
     </div>
     <div class="flex flex-col gap-6 mx-20">
         {#each notifications as notification}
-            {#if notification.isRead}
+            {#if notification.status === "read"}
                 <!-- svelte-ignore a11y_click_events_have_key_events -->
                 <!-- svelte-ignore a11y_no_static_element_interactions -->
-                <div class="flex items-center bg-[#F2F2F2] rounded-lg shadow-md p-4" onclick={() => markAsRead(notification)}>
+                <div class="flex items-center bg-[#F2F2F2] rounded-lg shadow-md p-4">
                     <img src="/images/bell.svg" alt="bell" class="w-10 h-10 mr-4"/>
                     <p class="text-lg text-[#404040]">{notification.message}</p>
                 </div>
             {:else}
                 <!-- svelte-ignore a11y_click_events_have_key_events -->
                 <!-- svelte-ignore a11y_no_static_element_interactions -->
-                <div class="flex items-center bg-[#F2F2F2] rounded-lg shadow-md p-4" onclick={() => markAsRead(notification)}>
+                <div class="flex items-center bg-[#F2F2F2] rounded-lg shadow-md p-4 cursor-pointer" onclick={() => markAsRead(notification.id)}>
                     <img src="/images/redBell.svg" alt="bell" class="w-10 h-10 mr-4" />
                     <p class="text-lg text-[#404040]">{notification.message}</p>
                 </div>
--- src/routes/tenant/userprofile/+page.svelte
+++ src/routes/tenant/userprofile/+page.svelte
@@ -1,126 +1,170 @@
 <script>
-    import { onMount } from "svelte";
-  
-    const Api_url = "http://localhost:3000";
-    let user = $state({});
-    let profile = $state({});
-    let history = [
-      { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
-      { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
-      { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
-      { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
-      { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
-      { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
-    ];
-  
-    let isEditing = $state(false);
-    let tempName = $state("ปทิตตา ดวงแก้ว");
-    let tempEmail = $state("patittadua@gmail.com");
-    let tempPassword = $state("Pramandanijjanukor2546");
-  
-    onMount(async () => {
-      try {
-        const res = await fetch(`${Api_url}/api/session`, {
-          method: "GET",
-          credentials: "include",
-        });
-        user = await res.json();
-        profile = user.profile;
-        tempName = profile?.name;
-        tempEmail = user?.email || "Enter Email";
-        tempPassword = user?.password || "Enter Password";
-      } catch (error) {
-        console.error("Error fetching message:", error);
-      }
-    });
-  
-    function toggleEdit() {
-      if (!isEditing) {
-        tempName = profile?.name;
-        tempEmail = user?.email;
-        tempPassword = user?.password;
-      } else {
-        if (profile) {
-          profile.name = tempName;
-        }
-        if (user) {
-          user.email = tempEmail;
-          user.password = tempPassword;
-        }
-      }
-      isEditing = !isEditing;
+  import { onMount } from "svelte";
+
+  const Api_url = "http://localhost:3000";
+  let user = $state({});
+  let profile = $state({});
+  let history = [
+    { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+    { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+    { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+    { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+    { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+    { month: "มกราคม 2567", amount: "8,324 บาท", status: "ชำระแล้ว" },
+  ];
+
+  let isEditing = $state(false);
+  let tempName = $state("ปทิตตา ดวงแก้ว");
+  let tempEmail = $state("patittadua@gmail.com");
+  let tempPassword = $state("Pramandanijjanukor2546");
+
+  onMount(async () => {
+    try {
+      const res = await fetch(`${Api_url}/api/session`, {
+        method: "GET",
+        credentials: "include",
+      });
+      user = await res.json();
+      profile = user.profile;
+      tempName = profile?.name;
+      tempEmail = user?.email || "Enter Email";
+      tempPassword = user?.password || "Enter Password";
+    } catch (error) {
+      console.error("Error fetching message:", error);
     }
-  </script>
-  
-  <div class="w-full min-h-full relative">
-    <div class="w-full h-full flex flex-col items-center justify-center">
-      <div class="w-full pt-16 flex flex-col items-center px-10">
-        <img
-          src="/images/Icon.svg"
-          alt="icon"
-          class="mb-6 w-24 h-24 fill-[#F2F2F2]"
+  });
+
+  function toggleEdit() {
+    if (!isEditing) {
+      tempName = profile?.name;
+      tempEmail = user?.email;
+      tempPassword = user?.password;
+    } else {
+      if (profile) {
+        profile.name = tempName;
+      }
+      if (user) {
+        user.email = tempEmail;
+        user.password = tempPassword;
+      }
+    }
+    isEditing = !isEditing;
+  }
+
+  async function save() {
+    try {
+      const res = await fetch(`${Api_url}/api/update/profile`, {
+        method: "PUT",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: tempName,
+          email: tempEmail,
+          password: tempPassword,
+        }),
+      });
+      const data = await res.json();
+      if (data.message === "อัพเดทโปรไฟล์สำเร็จ") {
+        toggleEdit();
+      }
+    } catch (error) {
+      console.error("Error fetching message:", error);
+    }
+  }
+</script>
+
+<div class="w-full min-h-full relative">
+  <div class="w-full h-full flex flex-col items-center justify-center">
+    <div class="w-full pt-16 flex flex-col items-center px-10">
+      <img
+        src="/images/Icon.svg"
+        alt="icon"
+        class="mb-6 w-24 h-24 fill-[#F2F2F2]"
+      />
+      {#if isEditing}
+        <input
+          class="mb-4 py-1 px-6 rounded-xl border-2 border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal"
+          bind:value={tempName}
+          placeholder="Enter Name"
         />
+      {:else}
+        <p class="text-[#F2F2F2] font-normal font-jeju text-4xl">{tempName}</p>
+      {/if}
+
+      {#if isEditing}
+        <input
+          class="py-1 px-6 rounded-xl border-2 border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal"
+          bind:value={tempEmail}
+          placeholder="Enter Email"
+        />
+      {:else}
+        <p class="text-[#F2F2F2] font-normal font-jeju text-2xl mb-10">
+          {tempEmail}
+        </p>
+      {/if}
+
+      <div class="w-full max-w-2xl text-left mb-10 relative mr-24">
+        <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-4">
+          รหัสผ่าน
+        </p>
         {#if isEditing}
-          <input class="mb-4 py-1 px-6 rounded-xl border-2  border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempName} placeholder="Enter Name"/>
+          <input
+            class="mb-4 w-full py-1 px-6 rounded-xl border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal"
+            bind:value={tempPassword}
+            placeholder="Enter Password"
+          />
         {:else}
-          <p class="text-[#F2F2F2] font-normal font-jeju text-4xl">{tempName}</p>
+          <p class="text-[#F2F2F2] text-xl mb-4 pl-10">{tempPassword}</p>
         {/if}
-  
-        {#if isEditing}
-          <input class="py-1 px-6 rounded-xl border-2 border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempEmail} placeholder="Enter Email"/>
-        {:else}
-          <p class="text-[#F2F2F2] font-normal font-jeju text-2xl mb-10">
-            {tempEmail}
-          </p>
-        {/if}
-  
-        <div class="w-full max-w-2xl text-left mb-10 relative mr-24">
-          <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-4">
-            รหัสผ่าน
-          </p>
-          {#if isEditing}
-            <input class="mb-4 w-full py-1 px-6 rounded-xl border-[#404040] bg-[#F2F2F2] text-left placeholder-[#8B8B8C] font-jeju text-xl font-normal" bind:value={tempPassword} placeholder="Enter Password"/>
-          {:else}
-            <p class="text-[#F2F2F2] text-xl mb-4 pl-10">{tempPassword}</p>
-          {/if}
-          <div
-            class="absolute bottom-0 left-0 w-full border-t-2"
-          ></div>
-        </div>
-        <div class="w-full max-w-3xl">
-          <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-6">
-            ประวัติการเช่า
-          </p>
-          <div class="grid grid-cols-2 gap-6">
-            {#each history as item}
+        <div class="absolute bottom-0 left-0 w-full border-t-2"></div>
+      </div>
+      <div class="w-full max-w-3xl">
+        <p class="text-[#F2F2F2] text-2xl font-normal leading-none mb-6">
+          ประวัติการเช่า
+        </p>
+        <div class="grid grid-cols-2 gap-6">
+          {#each history as item}
             <div class="bg-white rounded-xl shadow overflow-hidden p-2">
               <div class="flex">
-                <div><img src="/images/grayRect.svg" alt="" class="h-20"/></div>
+                <div>
+                  <img src="/images/grayRect.svg" alt="" class="h-20" />
+                </div>
                 <div class="flex flex-col justify-center">
-                  <div class="px-3 text-xl text-[#404040]">{item.month} ยอดรวม {item.amount}</div>
-                  <div
-                    class="px-3 text-xl text-[#404040]"
-                  >
+                  <div class="px-3 text-xl text-[#404040]">
+                    {item.month} ยอดรวม {item.amount}
+                  </div>
+                  <div class="px-3 text-xl text-[#404040]">
                     สภานะ: {item.status}
                   </div>
                 </div>
               </div>
             </div>
-            {/each}
-          </div>
+          {/each}
         </div>
       </div>
     </div>
-    <button onclick={toggleEdit} class="absolute bottom-12 right-12 border-none p-0">
-      {#if isEditing}
-          <div class="w-12 h-12 flex items-center justify-center">
-              <img src="/images/confirm.svg" alt="Confirm" class="w-8 h-8" />
-          </div>
-      {:else}
-          <div class="w-12 h-12 bg-[url('/images/editbg.svg')] bg-cover flex items-center justify-center">
-              <img src="/images/edit.svg" alt="Edit" class="w-8 h-8" />
-          </div>
-      {/if}
-  </button>
-  
   </div>
+  {#if isEditing}
+    <button
+      onclick={save}
+      class="absolute bottom-12 right-12 border-none p-0"
+    >
+      <div class="w-12 h-12 flex items-center justify-center">
+        <img src="/images/confirm.svg" alt="Confirm" class="w-8 h-8" />
+      </div>
+    </button>
+  {:else}
+    <button
+      onclick={toggleEdit}
+      class="absolute bottom-12 right-12 border-none p-0"
+    >
+      <div
+        class="w-12 h-12 bg-[url('/images/editbg.svg')] bg-cover flex items-center justify-center"
+      >
+        <img src="/images/edit.svg" alt="Edit" class="w-8 h-8" />
+      </div>
+    </button>
+  {/if}
+</div>
